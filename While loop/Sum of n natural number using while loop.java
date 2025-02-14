f5beeeef837d60f029d774aae92502b991322c42
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-02-14 17:08:42 +0530

    Create Sum of n natural number using while loop.java

---
+++ While loop/Sum of n natural number using while loop.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+     int sum=0;
+     int n=5;
+     int i=1;
+      while (i<=n) {
+          sum=sum+i;
+          i++;
+        }
+        System.out.print("Sum of n natural numbers "+ sum);
+    }
+}
