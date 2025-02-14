dec8ad58ad311c13260e76cc58afe6a21a00d5af
sharanya <116085931+sharanyazx@users.noreply.github.com>
2025-02-14 17:04:34 +0530

    Create Print all odd odd numbers using while loop.java

---
+++ While loop/Print all odd odd numbers using while loop.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+      int c = 20;
+      int i=1;
+      while (i<=c) {
+          if(i%2!=0) System.out.println(i);
+          i++; 
+        }
+    }
+}
